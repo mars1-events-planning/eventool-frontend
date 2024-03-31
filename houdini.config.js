d572d4256c8ebb280f14cfa50070372d18c03044
martynov <callmemars111@gmail.com>
2024-03-31 21:11:57 +0300

    change back and scp

--- houdini.config.js
+++ houdini.config.js
@@ -3,7 +3,7 @@
 /** @type {import('houdini').ConfigFile} */
 const config = {
     "watchSchema": {
-        "url": "http://localhost:5275/graphql"
+        "url": "https://backend.eventool.online/graphql"
     },
     "plugins": {
         "houdini-svelte": {
